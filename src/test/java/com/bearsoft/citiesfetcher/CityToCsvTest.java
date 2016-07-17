5ce6ad88eda604dcdef9e9701b61a058eb193c5e
marat-gainullin <mg@altsoft.biz>
2016-07-17 11:03:57 +0300

    CityToCsvTest enhancement

--- src/test/java/com/bearsoft/citiesfetcher/CityToCsvTest.java
+++ src/test/java/com/bearsoft/citiesfetcher/CityToCsvTest.java
@@ -1,35 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bearsoft.citiesfetcher;
 
 import com.bearsoft.citiesfetcher.model.City;
-import java.io.IOException;
+import com.bearsoft.citiesfetcher.transforms.CityToCsv;
 import java.util.function.Supplier;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
+ * Test suite for {@code CityToCsv} transformer.
  *
  * @author mg
  */
 public class CityToCsvTest {
 
+    /**
+     * Tests a case when no symbols to escape found in values.
+     */
     @Test
-    public void whenUnescapedTest() throws IOException {
+    public final void whenUnescapedTest() {
         City item = new City(1, " Wonderful city", "location", -20.2, 54);
         Supplier<StringBuilder> transformer = new CityToCsv(item);
         StringBuilder line = transformer.get();
-        assertEquals("1, Wonderful city,location,-20.2,54\r\n", line.toString());
+        assertEquals(
+                "1, Wonderful city,location,-20.2,54\r\n", line.toString());
     }
 
+    /**
+     * Tests a case when symbols to escape found in values.
+     */
     @Test
-    public void whenEscapedTest() throws IOException {
-        City item = new City(1, "Wonderful, \"city\"", "loc\r\nation", -20.2, 54);
+    public final void whenEscapedTest() {
+        City item = new City(
+                1,
+                "Wonderful, \"city\"", "loc\r\nation", -20.2, 54);
         Supplier<StringBuilder> transformer = new CityToCsv(item);
         StringBuilder line = transformer.get();
-        assertEquals("1,\"Wonderful, \"\"city\"\"\",\"location\",-20.2,54\r\n", line.toString());
+        assertEquals("1,\"Wonderful, \"\"city\"\"\",\"location\",-20.2,54\r\n",
+                line.toString());
     }
 }

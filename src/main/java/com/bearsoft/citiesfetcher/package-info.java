f24536f7ac8bad4f3b1258d7cfb5c0908414da38
marat-gainullin <mg@altsoft.biz>
2016-07-17 10:44:45 +0300

    City transformation to csv test adeed.

---
+++ src/main/java/com/bearsoft/citiesfetcher/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Cities tranformer base package.
+ * It consists of general purpose classes.
+ */
+package com.bearsoft.citiesfetcher;
--- src/test/java/com/bearsoft/citiesfetcher/CityCsvTransformerTest.java
+++ src/test/java/com/bearsoft/citiesfetcher/CityCsvTransformerTest.java
@@ -5,8 +5,8 @@
  */
 package com.bearsoft.citiesfetcher;
 
+import com.bearsoft.citiesfetcher.model.City;
 import java.io.IOException;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -19,17 +19,17 @@ public class CityCsvTransformerTest {
 
     @Test
     public void whenUnescapedTest() throws IOException {
-        City item = new City(1, "Wonderful city", "location", -20.2, 54);
+        City item = new City(1, " Wonderful city", "location", -20.2, 54);
         Supplier<StringBuilder> transformer = new CityCsvTransformer(item);
         StringBuilder line = transformer.get();
-        assertEquals("1,Wonderful city,location,-20.2,54", line.toString());
+        assertEquals("1, Wonderful city,location,-20.2,54\r\n", line.toString());
     }
-    
+
     @Test
     public void whenEscapedTest() throws IOException {
         City item = new City(1, "Wonderful, \"city\"", "loc\r\nation", -20.2, 54);
         Supplier<StringBuilder> transformer = new CityCsvTransformer(item);
         StringBuilder line = transformer.get();
-        assertEquals("1,\"Wonderful, \"\"city\"\"\",\"location\",-20.2,54", line.toString());
+        assertEquals("1,\"Wonderful, \"\"city\"\"\",\"location\",-20.2,54\r\n", line.toString());
     }
 }
